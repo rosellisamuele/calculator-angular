ee145db0ffb85b3e4727b53da302dee46cb3027f
lai2-24 <lai2-24@itismeucci.com>
2024-03-13 07:39:35 +0000

    Created angular project

---
+++ calculator-angular/src/app/calc-keyboard/calc-keyboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'calc-keyboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc-keyboard.component.html',
+  styleUrl: './calc-keyboard.component.css'
+})
+export class CalcKeyboardComponent {
+
+}
+
